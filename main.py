11fa5d8d44e058e0b6ba4eb0b90be78ad4b92368
blackHoleQp <undertalesyt@gmail.com>
2025-10-17 02:24:55 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from resources.install_packages import installing_all
-from resources.moving_configs import moving_all_configs
-
-if __name__ == '__main__':
-    installing_all()
-    moving_all_configs()
